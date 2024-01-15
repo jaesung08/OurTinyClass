4990fe49f2b15656b77c9328c5a087950d78c081
ChoiChangho <chh9450@naver.com>
2024-01-15 15:55:22 +0900

    [BE] feat: Initial commit

---
+++ be/src/main/java/com/otc/tinyclassroom/TinyClassroomApplication.java
@@ -0,0 +1,13 @@
+package com.otc.tinyclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyClassroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyClassroomApplication.class, args);
+	}
+
+}
