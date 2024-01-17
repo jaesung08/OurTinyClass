1b7f61d3c077e8c3779be9bd2dc7e83808fec87d
chh9450 <chh9450@naver.com>
2024-01-17 10:56:09 +0900

    feat : 헤더 부분 가이드 작성 및 로고 추가
    게시판 헤더 부분 틀 작성 및 로고를 추가했다.
    Close #88

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,5 +1,5 @@
 import Login from "./feature/auth/login/views/Login";
-import ArticlePage from "./pages/articlePage";
+import ArticlePage from "./feature/article/ArticlePage";
 
 export default function App() {
   return (
--- fe/src/feature/article/ArticlePage.tsx
+++ fe/src/feature/article/ArticlePage.tsx
@@ -1,10 +1,10 @@
-import SideBar from "@/components/SideBar";
-import Header from "@/components/Header";
+import SideBar from "@/components/Layout/SideBar";
+import Header from "./components/Header";
 
 function ArticlePage() {
   return (
     <section className="w-screen min-h-screen flex">
-      {/* <SideBar /> */}
+      <SideBar />
       <article className="w-11/12 bg-black min-h-screen">
         <Header />
       </article>
