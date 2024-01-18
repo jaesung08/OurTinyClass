b6dcbff19326ca783e2779a4204865be46811cbb
정서희 <jsh001505@naver.com>
2024-01-18 10:17:59 +0900

    init: storybook 도입
    컴포넌트 테스트를 위해 storybook 설치 및 적용

---
+++ fe/src/components/Elements/Spinner/Spinner.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from "@storybook/react";
+
+import { Spinner, SpinnerProps } from "./Spinner";
+
+const meta: Meta = {
+  title: "Components/Elements/Spinner",
+  component: Spinner,
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+export default meta;
+
+const Template: Story<SpinnerProps> = (props: SpinnerProps) => (
+  <Spinner {...props} />
+);
+
+export const Default = Template.bind({});
+Default.args = {};
