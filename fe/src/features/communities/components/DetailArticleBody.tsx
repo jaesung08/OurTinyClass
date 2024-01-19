c2ce864e7a787cfda2beb5e6a9381ec41e94fa0c
chh9450 <chh9450@naver.com>
2024-01-19 16:55:26 +0900

    feat : 게시글 수정 페이지 고침
    - 게시글 수정에서 수정 버튼과 완료 버튼을 누르면 수정되게 변함
    - 사소한 부분은 좀 더 수정이 필요함
        - quil 값 받아오는 부분 좀 더 확인 필요
        - state 사용해서 관리 좀 더 세세히 수정
        - API 받아서 게시판 완성하기

--- fe/src/features/communities/components/DetailArticleBody.tsx
+++ fe/src/features/communities/components/DetailArticleBody.tsx
@@ -15,10 +15,43 @@ import { useState } from "react";
 import ReactQuill from "react-quill";
 import Comments from "./Comments";
 import CommentsList from "./CommentsList";
+import { input } from "@testing-library/user-event/dist/cjs/event/input.js";
 
 function DetailArticleBody() {
-  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [isArticleEdit, setIsArticleEdit] = useState<boolean>(false);
+  const [articleContent, setArticleContent] = useState<string>(
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor beatae ipsa sit maxime commodi quas accusamus exercitationem obcaecati vitae unde illum, dolores totam, ratione a? Unde aspernatur porro sapiente quae? Placeat cum quam omnis totam unde quia beatae! Optio rerum ratione minima itaque beatae cumque aliquam a alias voluptatibus doloremque, earum, cum distinctio, eos ea? Vero dolor excepturi fuga hic. Excepturi quod fuga ducimus hic exercitationem, fugit quos tempora a ad eius, impedit, odit placeat sequi nisi natus non voluptas labore harum esse necessitatibus? Laudantium, nisi mollitia! Blanditiis, corporis autem. Cum reprehenderit illo a explicabo, veniam iusto! Earum corrupti accusamus quas expedita itaque saepe laboriosam rerum, numquam temporibus assumenda eum, quibusdam nisi ipsum animi laborum est tempore! Natus, obcaecati adipisci! Facilis iure consectetur aspernatur obcaecati reiciendis ut minima, nihil eligendi nobis placeat, blanditiis ratione amet doloremque quo consequatur non eius, saepe id hic nesciunt vero? Nostrum animi quae eaque nisi? beatae ipsa sit maxime commodi quas accusamus exercitationem obcaecati vitae unde illum, dolores totam, ratione a? Unde aspernatur porro sapiente quae? Placeat cum quam omnis totam unde quia beatae! Optio rerum ratione minima itaque beatae cumque aliquam a alias voluptatibus doloremque, earum, cum distinctio, eos ea? Vero dolor excepturi fuga hic. Excepturi quod fuga ducimus hic exercitationem, fugit quos tempora a ad eius, impedit, odit placeat sequi nisi natus non voluptas labore harum esse necessitatibus? Laudantium, nisi mollitia! Blanditiis, corporis autem. Cum reprehenderit illo a explicabo, veniam iusto! Earum corrupti accusamus quas expedita itaque saepe laboriosam rerum, numquam temporibus assumenda eum, quibusdam nisi ipsum animi laborum est tempore! Natus, obcaecati adipisci! Facilis iure consectetur aspernatur obcaecati reiciendis ut minima, nihil eligendi nobis placeat, blanditiis ratione amet doloremque quo consequatur non eius, saepe id hic nesciunt vero? Nostrum animi quae eaque nisi?"
+  );
 
+  const modules = {
+    toolbar: [
+      ["link", "image", "video"],
+      [{ header: [1, 2, 3, false] }],
+      ["bold", "italic", "underline", "strike"],
+      ["blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ color: [] }, { background: [] }],
+      [{ align: [] }],
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  };
+
+  const editArticle = () => {
+    setIsArticleEdit(!isArticleEdit);
+  };
+  const checkValue = (e) => {
+    console.log(e);
+    const check = e.split("<p>");
+    const textValue = check[1].split("</p>");
+    console.log(textValue);
+    setArticleContent(textValue[0]);
+  };
+  const completeArticleEdit = () => {
+    setIsArticleEdit(!isArticleEdit);
+  };
   return (
     <article className="w-10/12 h-full border-l-1">
       <div className="w-full bg-lime-500 flex items-center justify-center h-20"></div>
@@ -47,51 +80,40 @@ function DetailArticleBody() {
             </div>
           </div>
           <div className="w-2/12">
-            <Button
-              color="success"
-              className="w-1/12 text-white text-xl py-7 shadow-xl rounded-xl"
-            >
-              수정
-            </Button>
+            {isArticleEdit ? (
+              <Button
+                color="success"
+                className="w-1/12 text-white text-xl py-7 shadow-xl rounded-xl"
+                onClick={completeArticleEdit}
+              >
+                완료
+              </Button>
+            ) : (
+              <Button
+                color="success"
+                className="w-1/12 text-white text-xl py-7 shadow-xl rounded-xl"
+                onClick={editArticle}
+              >
+                수정
+              </Button>
+            )}
           </div>
         </div>
-        <div className="w-10/12">
-          <p className="w-full py-20 pl-10">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor
-            beatae ipsa sit maxime commodi quas accusamus exercitationem
-            obcaecati vitae unde illum, dolores totam, ratione a? Unde
-            aspernatur porro sapiente quae? Placeat cum quam omnis totam unde
-            quia beatae! Optio rerum ratione minima itaque beatae cumque aliquam
-            a alias voluptatibus doloremque, earum, cum distinctio, eos ea? Vero
-            dolor excepturi fuga hic. Excepturi quod fuga ducimus hic
-            exercitationem, fugit quos tempora a ad eius, impedit, odit placeat
-            sequi nisi natus non voluptas labore harum esse necessitatibus?
-            Laudantium, nisi mollitia! Blanditiis, corporis autem. Cum
-            reprehenderit illo a explicabo, veniam iusto! Earum corrupti
-            accusamus quas expedita itaque saepe laboriosam rerum, numquam
-            temporibus assumenda eum, quibusdam nisi ipsum animi laborum est
-            tempore! Natus, obcaecati adipisci! Facilis iure consectetur
-            aspernatur obcaecati reiciendis ut minima, nihil eligendi nobis
-            placeat, blanditiis ratione amet doloremque quo consequatur non
-            eius, saepe id hic nesciunt vero? Nostrum animi quae eaque nisi?
-            beatae ipsa sit maxime commodi quas accusamus exercitationem
-            obcaecati vitae unde illum, dolores totam, ratione a? Unde
-            aspernatur porro sapiente quae? Placeat cum quam omnis totam unde
-            quia beatae! Optio rerum ratione minima itaque beatae cumque aliquam
-            a alias voluptatibus doloremque, earum, cum distinctio, eos ea? Vero
-            dolor excepturi fuga hic. Excepturi quod fuga ducimus hic
-            exercitationem, fugit quos tempora a ad eius, impedit, odit placeat
-            sequi nisi natus non voluptas labore harum esse necessitatibus?
-            Laudantium, nisi mollitia! Blanditiis, corporis autem. Cum
-            reprehenderit illo a explicabo, veniam iusto! Earum corrupti
-            accusamus quas expedita itaque saepe laboriosam rerum, numquam
-            temporibus assumenda eum, quibusdam nisi ipsum animi laborum est
-            tempore! Natus, obcaecati adipisci! Facilis iure consectetur
-            aspernatur obcaecati reiciendis ut minima, nihil eligendi nobis
-            placeat, blanditiis ratione amet doloremque quo consequatur non
-            eius, saepe id hic nesciunt vero? Nostrum animi quae eaque nisi?
-          </p>
-        </div>
+        {isArticleEdit ? (
+          <div className="w-10/12">
+            <ReactQuill
+              className="w-full py-20 pl-10"
+              modules={modules}
+              value={articleContent}
+              onChange={(e) => checkValue(e)}
+            />
+          </div>
+        ) : (
+          <div className="w-10/12">
+            <p className="w-full py-20 pl-10">{articleContent}</p>
+          </div>
+        )}
+
         <div className="w-full mx-10">
           <Comments />
           <CommentsList />
