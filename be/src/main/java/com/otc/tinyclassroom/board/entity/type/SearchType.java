8441ef3f29f98577bb0379b832b507a7e17ad6d2
ParkJaeseon <wotjs61@naver.com>
2024-01-22 02:46:43 +0900

    feat: #83 - ENUM에 String으로 부터 type으로 변환하는 함수 추가
    게시판 검색시 url에 소문자로 검색할 수 있도록 변환 로직 추가

--- be/src/main/java/com/otc/tinyclassroom/board/entity/type/SearchType.java
+++ be/src/main/java/com/otc/tinyclassroom/board/entity/type/SearchType.java
@@ -1,5 +1,15 @@
 package com.otc.tinyclassroom.board.entity.type;
 
 public enum SearchType {
-    TITLE, CONTENT, NAME, TITLE_CONTENT
+    TITLE, CONTENT, NAME, TITLE_CONTENT;
+
+    // 대소문자 구별 없이 받아주기 위한 함수
+    public static SearchType fromString(String text) {
+        for (SearchType type : SearchType.values()) {
+            if (type.name().equalsIgnoreCase(text)) {
+                return type;
+            }
+        }
+        return null;
+    }
 }
