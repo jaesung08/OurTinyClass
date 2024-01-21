ee03471c2532a0e60457bee25cb469386fbdded8
ParkJaeseon <wotjs61@naver.com>
2024-01-22 02:47:57 +0900

    feat: #83 - 검색, 조회, 상세조회 구현

--- be/src/main/java/com/otc/tinyclassroom/board/repository/ArticleRepository.java
+++ be/src/main/java/com/otc/tinyclassroom/board/repository/ArticleRepository.java
@@ -1,13 +1,45 @@
 package com.otc.tinyclassroom.board.repository;
 
 import com.otc.tinyclassroom.board.entity.Article;
+import com.otc.tinyclassroom.board.entity.QArticle;
+import com.otc.tinyclassroom.board.entity.type.ArticleType;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 
 /**
  * 게시글 관련 Repository.
  */
 @Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long>,
+    QuerydslPredicateExecutor<Article>,
+    QuerydslBinderCustomizer<QArticle> {
 
+    // 전체 커뮤니티
+    Page<Article> findByClassRoomIsNullAndArticleTypeAndTitleContaining(ArticleType articleType, String title, Pageable pageable);
+
+    Page<Article> findByClassRoomIsNullAndArticleTypeAndContentContaining(ArticleType articleType, String content, Pageable pageable);
+
+    Page<Article> findByClassRoomIsNullAndArticleTypeAndTitleContainingOrContentContaining(ArticleType articleType, String title, String content, Pageable pageable);
+
+    Page<Article> findByClassRoomIsNullAndArticleTypeAndMember_NameContaining(ArticleType articleType, String name, Pageable pageable);
+
+    // 전체 게시판은 classRoom이 할당되지 않음
+    Page<Article> findByClassRoomIsNullAndArticleType(ArticleType articleType, Pageable pageable);
+
+    // 반 별 게시판
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        // 특정 property에 대한 검색 열기
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.member.name);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.member.name).first(StringExpression::containsIgnoreCase);
+    }
 }
--- be/src/main/java/com/otc/tinyclassroom/board/service/ArticleService.java
+++ be/src/main/java/com/otc/tinyclassroom/board/service/ArticleService.java
@@ -1,8 +1,39 @@
 package com.otc.tinyclassroom.board.service;
 
+import com.otc.tinyclassroom.board.dto.ArticleDto;
+import com.otc.tinyclassroom.board.entity.type.ArticleType;
+import com.otc.tinyclassroom.board.entity.type.SearchType;
+import com.otc.tinyclassroom.board.repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class ArticleService {
 
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(ArticleType articleType, SearchType searchType, String searchKeyword, Pageable pageable) {
+        if (searchKeyword == null || searchKeyword.isBlank()) {
+            return articleRepository.findByClassRoomIsNullAndArticleType(articleType, pageable).map(ArticleDto::from);
+        }
+
+        return switch (searchType) {
+            case TITLE -> articleRepository.findByClassRoomIsNullAndArticleTypeAndTitleContaining(articleType, searchKeyword, pageable).map(ArticleDto::from);
+            case CONTENT -> articleRepository.findByClassRoomIsNullAndArticleTypeAndContentContaining(articleType, searchKeyword, pageable).map(ArticleDto::from);
+            case TITLE_CONTENT -> articleRepository.findByClassRoomIsNullAndArticleTypeAndTitleContainingOrContentContaining(articleType, searchKeyword, searchKeyword, pageable).map(ArticleDto::from);
+            case NAME -> articleRepository.findByClassRoomIsNullAndArticleTypeAndMember_NameContaining(articleType, searchKeyword, pageable).map(ArticleDto::from);
+        };
+    }
+
+    public ArticleDto getArticle(Long articleId) {
+        return articleRepository.findById(articleId).map(ArticleDto::from)
+            .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다."));
+    }
 }
