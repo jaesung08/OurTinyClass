53f719f21ff23a8b5caa709908a14c6c955d50aa
LireEruel <jsh001505@gmail.com>
2024-01-21 21:50:16 +0900

    feat: #96 시간표 CSS 추가
    main dashboard의 시간표의 CSS를 추가하였습니다.

--- pc-app/src/feature/users/routes/MainDashboard.tsx
+++ pc-app/src/feature/users/routes/MainDashboard.tsx
@@ -1,14 +1,6 @@
 import { Plan } from "@/feature/schedule";
 import { getCurrentDayName } from "@/utils/DateFormattingHelpers";
-import {
-  Avatar,
-  Badge,
-  Button,
-  Card,
-  CardFooter,
-  CardHeader,
-  Progress,
-} from "@nextui-org/react";
+import { Button, Card, CardFooter, CardHeader } from "@nextui-org/react";
 import * as dayjs from "dayjs";
 import { useState } from "react";
 function MainDashBoard() {
@@ -304,38 +296,36 @@ function MainDashBoard() {
 
   return (
     <div className="flex w-full">
-      <div className="bg-white min-h-screen w-5/6 p-4">
-        <header className="flex justify-between items-center mb-6">
+      <div className="bg-white min-h-screen w-5/6 ">
+        <header className="flex justify-between items-center mb-6 shadow w-full py-5 pl-24 pr-10">
           <h1 className="text-2xl font-bold">나만의 작은 코너</h1>
           <div className="flex space-x-2"></div>
         </header>
-        <main className="flex-col gap-8">
-          <p>
-            안녕하세요, <span>{userInfo.name}</span>님
+        <main className="flex-col gap-8 px-24 pb-8">
+          <p className="text-2xl py-5">
+            안녕하세요, <span className="font-semibold">{userInfo.name}</span>님
           </p>
-          <section className="flex gap-6">
-            <div className="bg-green-400 p-4 shadow flex space-y-4 rounded w-4/12 h-40">
-              <div className="flex flex-col w-full gap-3">
-                <h2 className="text-lg ">출석체크 현황</h2>
-                <div className="flex items-center space-x-2 justify-between w-full ">
-                  <div className="flex flex-col gap">
-                    <div className="w-3 bg-blue-500 h-1 rounded" />
-                    <span className="font-bold text-blue-500 text-lg mb-3">
-                      {todayDate.format("MM.DD")}
-                    </span>
-                    <span className="text-blue-500 text-xs">
-                      {getCurrentDayName(todayDate.day())?.fullKr ?? ""}
-                    </span>
+          <section className="flex gap-6 h-38">
+            <div className="bg-green-400 p-4 shadow flex flex-col gap-3 space-y-4 rounded w-4/12 h-full">
+              <h2 className="text-lg ">출석체크 현황</h2>
+              <div className="flex items-center space-x-2 justify-between w-full ">
+                <div className="flex flex-col gap">
+                  <div className="w-3 bg-blue-500 h-1 rounded" />
+                  <span className="font-bold text-blue-500 text-lg mb-3">
+                    {todayDate.format("MM.DD")}
+                  </span>
+                  <span className="text-blue-500 text-xs">
+                    {getCurrentDayName(todayDate.day())?.fullKr ?? ""}
+                  </span>
+                </div>
+                <div className="flex h-16 gap-4">
+                  <div className="p-2 flex flex-col items-center bg-zinc-500 justify-evenly">
+                    <span className="text-white h-8">08:58</span>
+                    <span className="text-white text-xs">정상 출석</span>
                   </div>
-                  <div className="flex h-16 gap-4">
-                    <div className="p-2 flex flex-col items-center bg-zinc-500 justify-evenly">
-                      <span className="text-white h-8">08:58</span>
-                      <span className="text-white text-xs">정상 출석</span>
-                    </div>
-                    <Button className="text-blue-500 bg-white h-full rounded-none w-8">
-                      출석체크
-                    </Button>
-                  </div>
+                  <Button className="text-blue-500 bg-white h-full rounded-none w-8">
+                    출석체크
+                  </Button>
                 </div>
               </div>
             </div>
@@ -343,7 +333,7 @@ function MainDashBoard() {
               <div className="bg-white p-4 rounded-lg shadow">
                 <h2 className="text-lg font-semibold mb-4">공지사항 4</h2>
                 <ul className="space-y-2">
-                  {dummyNotices.slice(0, 4).map((notice) => (
+                  {dummyNotices.slice(0, 3).map((notice) => (
                     <li id={notice.id.toString()} className="flex">
                       <span className="text-sm mr-4 text-green-500">
                         {notice.writer}
@@ -359,10 +349,10 @@ function MainDashBoard() {
             </div>
           </section>
         </main>
-        <div>
+        <div className="px-24">
           <div className="flex justify-between flex-col gap-5">
             <div className="flex justify-between">
-              <h3>나의 계획</h3>
+              <h3 className="text-2xl">나의 계획</h3>
               <div className="flex gap-3">
                 <span>
                   {`${planStartDate.format("MM월 DD일")} - ${planStartDate
@@ -385,15 +375,25 @@ function MainDashBoard() {
             </div>
             <div className="flex">
               <ul className="flex flex-col">
+                <li className=" border h-8 w-10"></li>
                 {Array.from({ length: 6 }, (_, i) => (
-                  <li key={i}>{i + 1}</li>
+                  <li key={i} className="border h-8 w-10 text-center">
+                    {i + 1}
+                  </li>
                 ))}
               </ul>
               <div className="flex">
                 {planList.map((plan) => (
-                  <ul className="flex flex-col">
+                  <ul className="flex flex-col ">
+                    <li className="border h-8 w-40 text-center">
+                      {`${dayjs(plan.date).format("YYYY-MM-DD")} (${
+                        getCurrentDayName(dayjs(plan.date).day())?.shortKr
+                      })`}
+                    </li>
                     {plan.scheduleList.map((schedule, index) => (
-                      <li key={index}>{schedule.title}</li>
+                      <li key={index} className="border h-8 w-40 text-center">
+                        {schedule.title}
+                      </li>
                     ))}
                   </ul>
                 ))}
@@ -409,10 +409,6 @@ function MainDashBoard() {
             <CardHeader>
               <div className="flex items-center justify-between w-full gap-3">
                 <h6 className="text-lg font-semibold">3학년 2반</h6>
-                <span className="text-sm text-gray-400">
-                  (종료 예정 : 45분)
-                  {/* TODO 시간표를 계산해서 종료 예정 시간을 넣어야 한다. FE의 몫*/}
-                </span>
               </div>
             </CardHeader>
             {/* TODO : 여기에 진행중인 수업의 썸네일 넣어야 한다. */}
@@ -422,9 +418,11 @@ function MainDashBoard() {
                 <p>● 34/40</p>{" "}
                 {/* TODO 여기에 ●은 아이콘으로 바꾸고, 34와 40은 현재 스케줄의 수업 관련 데이터로 바꿔야 한다.*/}
               </div>
-              <div className="flex justify-between items-center w-full">
-                <p>수업 종류 : </p>
-                <p>정규</p>
+              <div className="flex  items-center w-full">
+                <p>
+                  수업 종류 : <span> 정규 </span>
+                </p>
+
                 {/* TODO 수업 데이터로 전환해야 한다.*/}
               </div>
             </CardFooter>
