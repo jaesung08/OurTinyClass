fb1a9a9cc3ba7fb837dd2bf55d31ffad5431c4d0
정서희 <jsh001505@naver.com>
2024-01-22 13:42:14 +0900

    feat: #96 출석체크 관련 UI 처리
    출석체크 관련 UI 를 위한 처리를 추가하였습니다.

--- pc-app/src/routes/index.tsx
+++ pc-app/src/routes/index.tsx
@@ -3,9 +3,10 @@ import { useRoutes } from "react-router-dom";
 import { protectedRoutes } from "./protected";
 import { publicRoutes } from "./public";
 import Landing from "@/feature/misc/Landing";
+import Cookies from "js-cookie";
 
 export const AppRoutes = () => {
-  const auth = undefined;
+  const auth = Cookies.get("token");
   const defaultRoutes = [
     {
       path: "/",
