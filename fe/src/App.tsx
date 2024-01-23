4885feb5a9295d314044f20bc42b61cfc3051b33
chh9450 <chh9450@naver.com>
2024-01-23 16:18:09 +0900

    refact : 사용하지 않는 구성요소 정리
    - fe/src/app.tsx 에서 사용하지 않는 구성요소 삭제

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,5 @@
 import { AppProvider } from "@/providers/app";
 import { AppRoutes } from "@/routes";
-import DetailArticle from "./features/communities/routes/DetailArticle";
-import FreeBoardPage from "./features/communities/routes/FreeBoardPage";
 
 function App() {
   return (
