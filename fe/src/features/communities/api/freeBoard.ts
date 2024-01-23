faf4c562d05d4839c0ae13a7e63b4fa351be981f
chh9450 <chh9450@naver.com>
2024-01-23 09:13:15 +0900

    docs : fe => pc-app communities 폴더 이동
    - fe => pc-app 으로 폴더 이동함

--- fe/src/features/communities/api/freeBoard.ts
+++
@@ -1,9 +0,0 @@
-import { axios } from "@/lib/axios";
-
-export const freeBoard = async () => {
-  return await axios.get("/board", {
-    params: {
-      boardType: "notice",
-    },
-  });
-};
--- fe/src/features/communities/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./";
-export * from "./routes/FreeBoardPage";
-export * from "./routes/CreateArticle";
-export * from "./routes/DetailArticle";
