598791d998566919a284e3e8c5ca979a2e2a29fc
ChoiChangho <chh9450@naver.com>
2024-01-24 15:58:11 +0900

    fix: #98 - api 요청에 맞춤 Login 시에 필요했던 스펙을 api 에 맞추고 record 로 바꿈.

--- be/src/main/java/com/otc/tinyclassroom/member/dto/request/MemberLoginRequestDto.java
+++ be/src/main/java/com/otc/tinyclassroom/member/dto/request/MemberLoginRequestDto.java
@@ -2,8 +2,15 @@ package com.otc.tinyclassroom.member.dto.request;
 
 import lombok.Data;
 
-@Data
-public class MemberLoginRequestDto {
-    private String loginId;
-    private String password;
+/**
+ * Login 시 필요한 Dto.
+ */
+public record MemberLoginRequestDto(String memberId, String password) {
+
+    /**
+     * 팩토리 메서드.
+     */
+    public static MemberLoginRequestDto of(String memberId, String password) {
+        return new MemberLoginRequestDto(memberId, password);
+    }
 }
