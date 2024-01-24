5ab6ed3b27215b570efb3b880267e122c50da1c6
ChoiChangho <chh9450@naver.com>
2024-01-24 16:01:06 +0900

    feat: #98 - Login Response 설정 프론트에 RefreshToken 을 body 에 같이 보내주기 위함.

--- be/src/main/java/com/otc/tinyclassroom/global/security/jwt/LoginResponseDto.java
+++ be/src/main/java/com/otc/tinyclassroom/global/security/jwt/LoginResponseDto.java
@@ -5,9 +5,9 @@ import com.otc.tinyclassroom.member.entity.Role;
 /**
  * 로그인 시 Member 정보를 넘겨주기 위한 Dto.
  */
-public record LoginResponseDto(String name, String memberId, Role role, int point) {
+public record LoginResponseDto(String name, String memberId, int role, int point, String refreshToken) {
 
-    public static LoginResponseDto of(String name, String memberId, Role role, int point) {
-        return new LoginResponseDto(name, memberId, role, point);
+    public static LoginResponseDto of(String name, String memberId, Role role, int point, String refreshToken) {
+        return new LoginResponseDto(name, memberId, role.getValue(), point, refreshToken);
     }
 }
