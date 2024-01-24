31f3af886d3dbfd509fd7286e9c6df815532c5d1
ChoiChangho <chh9450@naver.com>
2024-01-24 16:04:13 +0900

    feat: #98 - `Role` 에 번호 부여 및 getter 설정 프론트에 전달되는 값이 int 로 처리되기 위해서 parameter 를 설정하고 getter 를 부여함

--- be/src/main/java/com/otc/tinyclassroom/member/entity/Role.java
+++ be/src/main/java/com/otc/tinyclassroom/member/entity/Role.java
@@ -1,5 +1,17 @@
 package com.otc.tinyclassroom.member.entity;
 
+import lombok.Getter;
+
+/**
+ * 사용자 ROLE 을 정의한 Enum 타입.
+ */
+@Getter
 public enum Role {
-    ROLE_NEWBIE, ROLE_ADMIN
+    ROLE_STUDENT(1), ROLE_TEACHER(2), ROLE_MENTOR(3), ROLE_ADMIN(4);
+
+    private final int value;
+
+    Role(int value) {
+        this.value = value;
+    }
 }
