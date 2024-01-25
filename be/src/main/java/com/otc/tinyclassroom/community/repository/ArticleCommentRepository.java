6057b8f200b4dfce0de12ef7a5c41efe9f646cbd
ChoiChangho <chh9450@naver.com>
2024-01-25 15:04:45 +0900

    feat : #111 - 댓글 생성시 id값 반환 댓글 생성시 id값 반환하도록 설정한다.

--- be/src/main/java/com/otc/tinyclassroom/community/repository/ArticleCommentRepository.java
+++ be/src/main/java/com/otc/tinyclassroom/community/repository/ArticleCommentRepository.java
@@ -1,6 +1,7 @@
 package com.otc.tinyclassroom.community.repository;
 
 import com.otc.tinyclassroom.community.entity.ArticleComment;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
+    ArticleComment save(ArticleComment articleComment);
 }
