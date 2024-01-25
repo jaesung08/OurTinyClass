86bd8e571020daa975ff8e96d263da453b246719
정서희 <jsh001505@naver.com>
2024-01-25 13:26:19 +0900

    config: #110 Dockerfile 추가
    추후 있을 도커 배포에 대비하여 Docker file 추가 및 관련 설정을 추가하였습니다.

--- pc-app/vite.config.ts
+++ pc-app/vite.config.ts
@@ -4,11 +4,14 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   server: {
     port: 3000,
+    host: "0.0.0.0",
   },
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
+  assetsInclude: ["**/*.jpg"],
 });
