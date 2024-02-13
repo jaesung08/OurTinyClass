39f8379f6ef11a3356510ac569ec1cc734bd8f7c
정서희 <jsh001505@naver.com>
2024-02-13 17:57:30 +0900

    feat: #kakao login API 연결
    kakao login API 및 테스트 완료
    
    Change-Id: I342a1a7eb6bc161b66d734debc0f08793cb386cc

--- pc-app/src/feature/auth/api/login.ts
+++ pc-app/src/feature/auth/api/login.ts
@@ -1,15 +1,10 @@
 import { commonAxios } from "@/lib/commonAxios";
 import { LoginResponse } from "..";
 
-export const login = (
-  memberId: string,
-  password: string
-): Promise<LoginResponse> => {
-  return commonAxios.post("/members/login", { memberId, password });
+export const login = (memberId: string, password: string): Promise<LoginResponse> => {
+	return commonAxios.post("/members/login", { memberId, password });
 };
 
-// kakaologin 후 kakao에서 accessToken을 발급 받고, 백엔드에 API를 호출할 때 사용하는 함수
 export const kakaoLogin = (accessToken: string): Promise<LoginResponse> => {
-  // TODO: 임의의 url과 data, responseType을 넣었으니 백엔드 구현에 맞게 수정할 것
-  return commonAxios.post("/members/kakaoLogin", { accessToken });
+	return commonAxios.post("/members/kakao", { accessToken });
 };
