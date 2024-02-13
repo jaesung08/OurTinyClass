c21b2641f0891882533c245ec316b2e3e506fb90
최근영 <ought119@gmail.com>
2024-02-13 10:49:18 +0900

    feat : #156 수강 신청
    - 수강 신청 항목 Listbox => Accordion 변경
    
    Change-Id: I7bc5d7795fbcaade4bb5812b6177c3b265bf8f1f

--- pc-app/src/feature/schedule/components/ScheduleModal.tsx
+++ pc-app/src/feature/schedule/components/ScheduleModal.tsx
@@ -1,61 +1,59 @@
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Input} from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Input } from "@nextui-org/react";
 import { useState } from "react";
 import { SelectCategories } from "../assets/textContents";
-import {Listbox, ListboxItem} from "@nextui-org/react";
-
+import { Accordion, AccordionItem } from "@nextui-org/react";
 
 type CreateScheduleProps = {
-    isOpen: boolean;
-    onOpenChange: (isOpen: boolean) => void;
-}
-export default function ScheduleModal({
-    isOpen,
-    onOpenChange,
-}: CreateScheduleProps
-) {
-    const [ todoSchedule, setTodoSchedule ] = useState<string>("")
-  return (
-    <>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">일정 추가</ModalHeader>
-              <ModalBody>
-              <Input type="text" label="할일" disabled placeholder="할 일을 고르세요" value={todoSchedule}/>
-                {/* <div className="flex flex-col w-full overflow-y-scroll h-80 gap-5 scrollbar-hide">
-                    {
-                        SelectCategories.map((item) => {
-                            return <p className="bg-white px-2 py-3 border-2 rounded-lg cursor-pointer" key={item.id} onClick={() => setTodoSchedule(item.name)}>{item.name}</p>
-                        })
-                    }
-                </div> */}
-    <p className="mt-5">일반 항목</p>
-    <div className="flex flex-col w-full overflow-y-scroll h-32 gap-5 scrollbar-hide">
-      <Listbox
-        aria-label="Actions"
-        onAction={(key) => setTodoSchedule(key)}
-      >
-        {
-          SelectCategories.map((item) => {
-            return <ListboxItem key={item.name}>{item.name}</ListboxItem>                          
-          })
-        }
-      </Listbox>
-      </div>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" onPress={onClose}>
-                  닫기
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  추가
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
-  );
+	isOpen: boolean;
+	onOpenChange: (isOpen: boolean) => void;
+};
+export default function ScheduleModal({ isOpen, onOpenChange }: CreateScheduleProps) {
+	const [todoSchedule, setTodoSchedule] = useState<string>("");
+	return (
+		<>
+			<Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+				<ModalContent>
+					{(onClose) => (
+						<>
+							<ModalHeader className="flex flex-col gap-1">일정 추가</ModalHeader>
+							<ModalBody>
+								<Input type="text" label="할일" disabled placeholder="할 일을 고르세요" value={todoSchedule} />
+								<p className="mt-5">일반 항목</p>
+								<div className="flex flex-col w-full overflow-y-scroll h-48 gap-5 scrollbar-hide">
+									{SelectCategories.map((item) => {
+										return (
+											<Accordion key={item.id}>
+												<AccordionItem
+													className="border-1 rounded-xl px-5"
+													key={item.id}
+													aria-label={`${item.id} ${item.id}`}
+													title={`${item.name}`}
+												>
+													{item.content.map((info) => {
+														return (
+															<p className="p-5 cursor-pointer" onClick={() => setTodoSchedule(info.title)} key={info.lectureId}>
+																{info.title}
+															</p>
+														);
+													})}
+												</AccordionItem>
+											</Accordion>
+										);
+									})}
+								</div>
+							</ModalBody>
+							<ModalFooter>
+								<Button color="danger" onPress={onClose}>
+									닫기
+								</Button>
+								<Button color="primary" onPress={onClose}>
+									추가
+								</Button>
+							</ModalFooter>
+						</>
+					)}
+				</ModalContent>
+			</Modal>
+		</>
+	);
 }
