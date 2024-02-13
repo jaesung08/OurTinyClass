cd92417e17fb7b636f0dfb7a54adb27479442393
최근영 <ought119@gmail.com>
2024-02-13 18:00:31 +0900

    refact : #156 스케줄 생성
    - 스케줄 생성 API 요청 중 테스트
    - BE에서 받아온 값 리스트 순회하는 부분 오류 발생
    
    Change-Id: Ifd5e43888dd14f6c38f725f2a25072a62048db99

--- pc-app/src/feature/schedule/components/ScheduleModal.tsx
+++ pc-app/src/feature/schedule/components/ScheduleModal.tsx
@@ -12,16 +12,49 @@ type CreateScheduleProps = {
 	fetchScheduleList: () => void;
 };
 
+type ResponseLecture = {
+	approved: string;
+	date: null;
+	dayofWeek: number;
+	description: string;
+	id: number;
+	lectureCategoryType: string;
+	lectureType: string;
+	member: {
+		name: string;
+		role: string;
+	};
+	timeTable: number;
+	title: string;
+};
+
 export default function ScheduleModal({ isOpen, onOpenChange, date, dayOfWeek, fetchScheduleList }: CreateScheduleProps) {
 	const [todoSchedule, setTodoSchedule] = useState<string>("");
 	const [currentLecture, setCurrentLecture] = useState<number>(1);
+	const [specialLecture, setSpecialLecture] = useState<object[]>([]);
+	const [freeLecture, setFreeLecture] = useState<object[]>([]);
+	const [categoryName, setCategoryName] = useState<string[]>([]);
 
 	useEffect(() => {
 		const getCategories = async () => {
 			try {
 				const responseSpecial = await getSpecialLecture(date, dayOfWeek);
-				const responseFree = await getFreeLecture(0);
-				console.log(responseFree, responseSpecial);
+				const responseFree = await getFreeLecture("FREE_LECTURE");
+				setSpecialLecture(responseSpecial.data);
+				// 카테고리 별 매핑
+				const groupedLectures = responseFree.data.reduce(
+					(categoryFreeLecture: { [category: string]: ResponseLecture[] }, lecture: ResponseLecture) => {
+						const category: string = lecture.lectureCategoryType;
+						if (!categoryFreeLecture[category]) {
+							categoryFreeLecture[category] = [];
+						}
+						categoryFreeLecture[category].push(lecture);
+						return categoryFreeLecture;
+					},
+					{}
+				);
+				setFreeLecture(groupedLectures);
+				setCategoryName(Object.keys(groupedLectures));
 			} catch (error) {
 				console.error(error);
 			}
@@ -54,21 +87,21 @@ export default function ScheduleModal({ isOpen, onOpenChange, date, dayOfWeek, f
 								</div>
 								<div className="flex">
 									<div className="flex flex-col w-full overflow-y-scroll h-52 gap-5 scrollbar-hide">
-										{SelectCategories.map((item) => {
+										{freeLecture.map((item, index) => {
 											return (
-												<Accordion key={item.id}>
+												<Accordion key={index}>
 													<AccordionItem
 														className="border-1 rounded-xl px-5"
-														key={item.id}
-														aria-label={`${item.id} ${item.id}`}
-														title={`${item.name}`}
+														key={`${index}`}
+														aria-label={`${index}`}
+														title={categoryName[index]}
 													>
-														{item.content.map((info) => {
+														{item.map((info) => {
 															return (
 																<p
 																	className="p-5 cursor-pointer"
 																	onClick={() => {
-																		setTodoSchedule(info.title);
+																		setTodoSchedule();
 																		setCurrentLecture(info.lectureId);
 																	}}
 																	key={info.lectureId}
