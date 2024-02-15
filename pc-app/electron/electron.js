055099739640cc2b0dbbedd0a7882d565794448b
정서희 <jsh001505@naver.com>
2024-02-15 17:12:56 +0900

    fix: #151 카카오 로그인 적용
    Change-Id: I90c901242ce99f3a39b5ad267c7b0c79b781c1b1

--- pc-app/electron/electron.js
+++ pc-app/electron/electron.js
@@ -6,44 +6,44 @@ const { app, BrowserWindow } = require("electron");
 const isDev = process.env.IS_DEV == "true" ? true : false;
 
 function createWindow() {
-	const mainWindow = new BrowserWindow({
-		width: 1400,
-		height: 1000,
-		autoHideMenuBar: true,
-		resizable: true,
-		frame: true,
-		webPreferences: {
-			preload: path.join(__dirname, "preload.js"),
-			nodeIntegration: true,
-			contextIsolation: false,
-			enableRemoteModule: true,
-		},
-	});
+  const mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 1000,
+    autoHideMenuBar: true,
+    resizable: true,
+    frame: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
 
-	mainWindow.webContents.setWindowOpenHandler((edata) => {
-		shell.openExternal(edata.url);
-		return { action: "deny" };
-	});
+  // mainWindow.webContents.setWindowOpenHandler((edata) => {
+  // 	shell.openExternal(edata.url);
+  // 	return { action: "deny" };
+  // });
 
-	mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../dist/index.html")}`);
+  mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../dist/index.html")}`);
 
-	// Open the DevTools.
-	if (isDev) {
-		mainWindow.webContents.openDevTools();
-	}
+  // Open the DevTools.
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 }
 
 app.whenReady().then(() => {
-	createWindow();
-	app.on("activate", function () {
-		if (BrowserWindow.getAllWindows().length === 0) createWindow();
-	});
+  createWindow();
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 
-	app.setAppUserModelId("우리들의 작은 교실");
+  app.setAppUserModelId("우리들의 작은 교실");
 });
 
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
